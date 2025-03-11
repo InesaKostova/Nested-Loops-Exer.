b376269febf080be41c7ec80f7bb9263be518058
Inesa Kostova <kostovainesa@gmail.com>
2025-03-11 14:49:09 +0200

    Add files via upload

---
+++ Sum Prime Non Prime.cs
@@ -0,0 +1,39 @@
+﻿string input = Console.ReadLine();
+int sumPrime = 0;
+int sumNonPrime = 0;
+
+while(input!="stop")
+{
+    int num = int.Parse(input);
+    if(num < 0 )
+    {
+        Console.WriteLine($"Number is negative.");
+        num = 0;
+    }
+    bool isPrime = true;
+    for(int i=2; i<=num-1;i++)
+    {
+        if(num==2)
+        {
+            break;
+        }
+    if(num % i==0)
+        {
+          isPrime = false;
+            break;
+        }
+    }
+    if(isPrime)
+    {
+        sumPrime += num;
+    }
+    else
+    {
+        sumNonPrime+= num;
+    }
+    input = Console.ReadLine();
+    isPrime = true;
+
+}
+Console.WriteLine($"Sum of all prime numbers is: {sumPrime}");
+Console.WriteLine($"Sum of all non prime numbers is: {sumNonPrime}");
